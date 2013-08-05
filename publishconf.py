bfecd159eb493d75e64fd17f3efe765e0b7a3c3f
sb <sbenthall@gmail.com>
2013-08-05 16:09:27 -0700

    switch to relative links

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEURL = 'courses.ischool.berkeley.edu/i290m-ocpp/site'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
