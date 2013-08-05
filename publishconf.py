7622066623783dae8ffa16f7d92e430e49479145
sb <sbenthall@gmail.com>
2013-08-05 16:04:53 -0700

    no, really, set the SITEURL for publishing

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
+SITEURL = 'courses.ischool.berkeley.edu/i290m-ocpp/site'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
